5ae2803f010c439c945a8d90e26430660c09909b
Bernard Leanse <bernardleanse96@gmail.com>
2022-06-28 12:30:48 +0100

    gets estonia live count

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
+from db_interaction import DBInteraction
 
 app = Flask(__name__)
 CORS(app)
+dbi = DBInteraction()
 
 @app.route("/number-of-submissions")
 def number_of_submissions():
-  return {"count": 44}
+  count = dbi.get_submissions_count("seta-est.cvcpj1fhj3k9.us-east-2.rds.amazonaws.com", "est")
+  return {"count": count}
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8000, debug=True)
