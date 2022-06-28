7d5dd56bef1e73592add86dd87e0938bc7327ece
Bernard Leanse <bernardleanse96@gmail.com>
2022-06-28 11:24:26 +0100

    number of submissions route

--- app.py
+++ app.py
@@ -4,12 +4,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
+@app.route("/number-of-submissions")
 def hello_world():
   return "<h1>Hello World</h1>"
-
-@app.route("/test-route")
-def test_route():
-  return "<h1>You are in the test route</h1>"
-
-  
