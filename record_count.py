6e7dda27c21e24cc203be40ad060416e1f4633cf
Bernard Leanse <bernardleanse96@gmail.com>
2022-06-29 11:52:51 +0100

    record counter

--- record_count.py
+++ record_count.py
@@ -1,2 +1,11 @@
+import time
+
 class RecordCount:
-  pass
+  def __init__(self):
+    self.counts = 300
+
+  def increment_count(self):
+    while True:
+      time.sleep(6)
+      self.counts += 1
+  
