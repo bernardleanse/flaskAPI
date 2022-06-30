5dff91efbab987a17e3e22f6ba935abd1c91cb43
Bernard Leanse <bernardleanse96@gmail.com>
2022-06-30 16:33:21 +0100

    mongo uri

--- mongo_config.py
+++ mongo_config.py
@@ -1,8 +1,9 @@
+import os
 import pymongo
 
 class MongoConnection:
   def __init__(self):
-    self.client = pymongo.MongoClient("mongodb+srv://bernard:GdpGCrjGfYyoFEdi@cluster0.xylne.mongodb.net/?retryWrites=true&w=majority")
+    self.client = pymongo.MongoClient(os.environ.get("MONGO_URI"))
     self.db = self.client.PisaDataWarehouse
     self.counts = self.db.counts
   
